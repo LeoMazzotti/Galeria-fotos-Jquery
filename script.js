ad797eef746db78f434eec0626c95d4dc7467806
Leonardo Mazzotti <leonardo.mazzotti@yahoo.com.br>
2024-03-05 15:02:42 -0300

    commit inicial

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
   $('form').on('submit', function (e) {
     e.preventDefault()
     const novaImagem = $('#input-image').val()
-    const novoElemento = $('<li style="display:none">  </li>')
+    const novoElemento = $('<li style="display:none"> </li>')
     $(`<img src ="${novaImagem}" />`).appendTo(novoElemento)
     $(`<div class="overlay-image-link"> 
     <a href="${novaImagem}" target=_"blank" title="Clique aqui para expandir a imagem">
@@ -19,7 +19,6 @@ $(document).ready(function () {
     </div>`).appendTo(novoElemento)
     $(novoElemento).appendTo('ul')
     $(novoElemento).fadeIn(1000)
-
     $('#input-image').val('')
   })
 })
